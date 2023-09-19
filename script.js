4e4035920b00018a304e93a4f24b768128645ec8
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-19 18:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let btn = document.getElementById("sum_btn");
+
+function sum(){
+	let num1 = document.getElementById("n1").value;
+	num1 = parseInt(num1);
+	let num2 = document.getElementById("n2").value;
+	num2 = parseInt(num2);
+	let sum = num1+num2;
+	document.getElementById("sum").innerHTML=sum;
+}
+
+btn.addEventListener("click",sum);
